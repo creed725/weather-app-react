80d4c2f76853a42ab3510ef2fa755e1521498338
Ciara R. <100184461+creed725@users.noreply.github.com>
2024-10-16 17:32:19 -0500

    Added axios integration so it shows the weather for a city

---
+++ src/Weather.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { RotatingLines } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <h2>
+      <RotatingLines
+        visible={true}
+        height="96"
+        width="96"
+        color="red"
+        strokeWidth="5"
+        animationDuration="0.75"
+        ariaLabel="rotating-lines-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+      />
+    </h2>
+  );
+}
